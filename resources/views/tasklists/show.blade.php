93f228772a8eaf1b0680fe8eb771841e6c156415
yukako.nishiyama <yukako.1995@gmail.com>
2018-05-29 07:05:24 +0000

    CRUD messages

---
+++ resources/views/tasklists/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>id = {{ $tasklist->id }} のタスク詳細ページ</h1>
+
+    <p>{{ $tasklist->content }}</p>
+
+
+    {!! link_to_route('tasklists.edit', 'このタスクを編集', ['id' => $tasklist->id]) !!}
+{!! Form::model($tasklist, ['route' => ['tasklists.destroy', $tasklist->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+@endsection
