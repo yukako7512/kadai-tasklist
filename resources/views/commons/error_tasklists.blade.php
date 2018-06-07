f3deb4d49237109417512329f2c16181b076d693
yukako.nishiyama <yukako.1995@gmail.com>
2018-06-07 06:04:36 +0000

    top page

--- resources/views/commons/error_tasklists.blade.php
+++ resources/views/commons/error_tasklists.blade.php
@@ -1,7 +1,7 @@
 @if (count($errors) > 0)
     <ul>
         @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
+            <div class="alert alert-warning">{{ $error }}</div>
         @endforeach
     </ul>
 @endif
