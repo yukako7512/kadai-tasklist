bf198f99faadf370aeb88093ee6bf566b07921c9
yukako.nishiyama <yukako.1995@gmail.com>
2018-06-08 05:48:23 +0000

    add Route

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => 'auth'], function () {
-   
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
     
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
