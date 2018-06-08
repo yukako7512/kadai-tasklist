7cbf500dc28bac40940e8c276c80bbda45db476f
yukako.nishiyama <yukako.1995@gmail.com>
2018-06-08 05:28:17 +0000

    add Auth to showw

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,5 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
     
-    
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
